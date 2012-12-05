4e47ef127ffcb6773dbe13d95cc08224f97bd4b3
Emil Bengtsson <emil@emil0r.com>
2012-12-05 16:03:18 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject eyespy "0.1.0-SNAPSHOT"
+  :description "CSS Reloader"
+  :url "http://emil0r.com/eyespy"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [lamina "0.5.0-beta8"]
+                 [aleph "0.3.0-beta7"]
+                 [fs "1.3.2"]])
---
+++ test/eyespy/core_test.clj
@@ -0,0 +1,7 @@
+(ns eyespy.core-test
+  (:use clojure.test
+        eyespy.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
