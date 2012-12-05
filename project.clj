2ed6f6e5f92786e2457aa472bd9ec431b18cd286
Emil Bengtsson <emil@emil0r.com>
2012-12-05 16:04:18 +0100

    removed lamina as it's in aleph

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lamina "0.5.0-beta8"]
                  [aleph "0.3.0-beta7"]
                  [fs "1.3.2"]])
