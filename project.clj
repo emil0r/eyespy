6e610ec021cf6a9d1e3ecc51d22ec5b9b1c413fd
Emil Bengtsson <emil@emil0r.com>
2012-12-07 16:34:00 +0100

    updating project.clj for standalone jars

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject eyespy "0.3.0-SNAPSHOT"
+(defproject eyespy "0.9.0"
   :description "CSS Reloader"
   :url "http://emil0r.com/eyespy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [aleph "0.3.0-beta7"]
-                 [fs "1.3.2"]])
+                 [fs "1.3.2"]]
+  :main eyespy.core)
