a48f94cad5cfdfe9a84d98365a2b7de2de0c26c4
Emil Bengtsson <emil@emil0r.com>
2013-04-09 13:17:07 +0200

    added support for settings file

--- src/eyespy/core.clj
+++ src/eyespy/core.clj
@@ -3,22 +3,34 @@
   (:require [aleph.http :as http]
             [clojure.java.io :as io]
             [fs.core :as fs]
-            [lamina.core :as lamina]))
+            [lamina.core :as lamina])
+  (:use [clojure.java.shell :only [sh]]))
 
 (def running (atom true))
+(def -actions (atom {}))
 
 (defn- return-files [files]
-  (doseq [missing-file (filter #(not (fs/file? %)) files)]
+  (doseq [missing-file (filter #(and (not (fs/file? %))
+                                     (not (fs/directory? %))) files)]
     (println missing-file "can't be found"))
   (atom (apply merge {} (map (fn [f] {f {:mod (fs/mod-time f) :changed? false}}) (filter fs/file? files)))))
 
+(defn- read-settings [file]
+  (let [{:keys [notify actions] :as data} (read-string (slurp file))]
+    (doseq [{:keys [command watch-dir]} actions]
+      (swap! -actions assoc command (return-files (map #(str watch-dir %) (fs/list-dir watch-dir)))))
+    (return-files notify)))
+
 (defn- watchable-files [args]
-  (if (= (first args) "--watch")
-    (if (fs/file? (second args))
-      (let [files (line-seq (io/reader (second args)))]
-        (return-files files))
-      (println "Missing argument to --watch"))
-    (return-files args)))
+  (case (first args)
+        "--watch" (if (fs/file? (second args))
+                    (let [files (line-seq (io/reader (second args)))]
+                      (return-files files))
+                    (println "Missing argument to --watch"))
+        "--settings" (if (fs/file? (second args))
+                       (read-settings (second args))
+                       (println "Missing argument to --settings"))
+        (return-files args)))
 
 (defn- watch-files [files]
   (loop [[f & r] (keys @files)
@@ -37,6 +49,11 @@
         (println f "changed")
         (lamina/enqueue channel f)))))
 
+(defn- run-actions []
+  (doseq [[command watch] @-actions]
+    (if (watch-files watch)
+      (sh command))))
+
 (defn- listener-handler [broadcast-channel]
   (fn [ch handshake]
     (println "New connection...")
@@ -49,20 +66,24 @@
 Try --watch <file> (line-seperated files to watch)
 or <file1> <file2> <file3> <etc>")
     (if @running
-     (let [_ (println "EyeSpy starting...")
-           files (watchable-files args)
-           broadcast-channel (lamina/permanent-channel)
-           server (http/start-http-server (listener-handler broadcast-channel) {:port 4321 :websocket true})]
-       (println "EyeSpy started...")
-       (while @running
-         (let [changed? (watch-files files)]
-           (if changed?
-             (notify-browsers files broadcast-channel))
-           (Thread/sleep 100)))
-       (println "Stopping EyeSpy...")
-       (server)
-       (println "Exiting EyeSpy...")))))
+      (do
+        (println "EyeSpy starting...")
+        (let [files (watchable-files args)
+              broadcast-channel (lamina/permanent-channel)
+              server (http/start-http-server (listener-handler broadcast-channel) {:port 4321 :websocket true})]
+          (println "EyeSpy started...")
+          (while @running
+            (run-actions)
+            (let [changed? (watch-files files)]
+              (if changed?
+                (notify-browsers files broadcast-channel))
+              (Thread/sleep 100)))
+          (println "Stopping EyeSpy...")
+          (server)
+          (println "Exiting EyeSpy..."))))))
 
-;; (reset! running false)
-;; (-main "../angularjs/css/main.css")
-;; (reset! running true)
+;;(reset! running false)
+;;(-main "--settings" "settings.clj")
+;;(read-settings "settings.clj")
+;;(reset! running true)
+
